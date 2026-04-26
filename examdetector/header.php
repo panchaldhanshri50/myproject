47ea57685b7f3e4bc5514eed02e23635839d0040
panchaldhanshri50 <panchaldhanshri50@gmail.com>
2026-04-26 12:47:46 +0530

    upload folder

---
+++ examdetector/header.php
@@ -0,0 +1,32 @@
+
+<style>
+    nav a:link{
+        text-decoration: none;
+    }
+    nav a:hover{
+        text-decoration: underline;
+    }
+    nav a:active{
+        text-decoration: underline;
+    }
+</style>
+
+<header style="background: linear-gradient(135deg, #4b6cb7 0%, #182848 100%); color:white; padding:5px 40px; display:flex; justify-content:space-between;">
+    
+    
+    <img src="img/Picture1.png" alt="logo" style="height: 80px; width: auto;">
+            
+
+    <nav style="display: flex; align-items: center; gap: 70px; margin-top: 30px;">
+        <a href="index.php" style="color:white; margin-left:20px;"><i class="fas fa-home"></i>Home</a>
+
+        
+            
+        
+            <!-- VISITOR LINKS -->
+            <a href="signup.php" style="color:white; margin-left:20px;">Register</a>
+            <a href="login.php" style="color:white; margin-left:20px;">Login</a>
+        
+    </nav>
+
+</header>
